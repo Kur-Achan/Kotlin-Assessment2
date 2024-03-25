480feb011991e066653a97637b2e0820f309b433
Kur-Achan <you@example.com>
2024-03-25 08:29:11 +0300

    add files

---
+++ src/Main.kt
@@ -0,0 +1,37 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    }
+fun sentence(sentence:String){
+    var sentence = ("Barnie bakes brown bangels and buns")
+    println(listOf(String))
+    }
+
+var numbers = ArrayOf(12,24,2,18){
+    println( numbers.average())
+    println(numbers. count())
+    println(numbers. average())
+}
+fun volume(volume:Int){
+var   Volume = "4/3 * 3.1459 * r * r * r"
+     var volume =" 4/3 * 3.1459 *r * r * r"
+}
+fun isPalindrome(word:String):Boolean{
+    var name = "Kayak"
+    println("Kayak")
+    if (name=="Kayak")
+        return true
+
+    else
+    {
+      return false
+    }
+
+
+
+    }
+
+
+
+
